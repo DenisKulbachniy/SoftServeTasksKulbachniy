420b9d3aeefdb6b58f2fc48c92450fbc467cce2c
Denis Kulbachniy <Denismerefa@gmail.com>
2020-10-21 23:40:15 +0300

    task2 modified

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,20 +1,27 @@
 package task2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int sumOfWords = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("How much lines u wanna have: ");
-        int quantityOfLines = sc.nextInt();
-        String array[] = new String[quantityOfLines];
-        for (int i = 0; i < quantityOfLines ; i++) {
-            System.out.println("Enter value");
-            String value = sc.next();
-            array[i] = value;
-            sumOfWords += array[i].length();
+
+        try {
+            System.out.println("How much lines u wanna have: ");
+
+            int quantityOfLines = sc.nextInt();
+            String array[] = new String[quantityOfLines];
+            for (int i = 0; i < quantityOfLines; i++) {
+                System.out.println("Enter value");
+                String value = sc.next();
+                    sumOfWords += array[i].length();
+                }
+                System.out.println("String average length is: " + sumOfWords / quantityOfLines);
         }
-        System.out.println("String average length is: " + sumOfWords/quantityOfLines);
+        catch (InputMismatchException er){
+            System.out.println("Can`t operate this");
+        }
     }
 }
